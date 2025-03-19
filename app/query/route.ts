914b2171b18ebd4df87205748ed8836e91576a98
Miguel Caldeira <mcaldeira@MeninaDesign.net>
2025-03-19 16:34:51 +0000

    Update routes.js

--- app/query/route.ts
+++ app/query/route.ts
@@ -7,15 +7,12 @@ const sql = postgres(process.env.POSTGRES_URL!, {
 async function listInvoices() {
   try {
     const data = await sql`
-      SELECT invoices.amount, customers.name
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      WHERE invoices.amount = 666;
+      SELECT * FROM invoices LIMIT 5;
     `;
     return data;
   } catch (error) {
     console.error('Erro ao buscar faturas:', error);
-    throw new Error('Erro ao buscar faturas');
+    throw new Error(error.message);
   }
 }
 
